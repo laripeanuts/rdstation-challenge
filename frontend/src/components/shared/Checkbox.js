382b7fc33329776e6b1e1032d0db08557eab40df
Ana Clara <larissarabelolf@gmail.com>
2025-11-01 18:42:33 -0300

    feat: :sparkles: allow dynamic input type for Checkbox component

--- frontend/src/components/shared/Checkbox.js
+++ frontend/src/components/shared/Checkbox.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Checkbox({ children, ...props }) {
+function Checkbox({ children, type = 'checkbox', ...props }) {
   return (
     <label className="flex items-center">
-      <input type="checkbox" className="form-checkbox h-5 w-5 text-blue-500" {...props} />
+      <input type={type} className="form-checkbox h-5 w-5 text-blue-500" {...props} />
       <span className="ml-2">{children}</span>
     </label>
   );
